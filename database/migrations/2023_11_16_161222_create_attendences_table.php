8bfa97b4d8ec2f63ff4c249a26f5762bdf21ec36
Jurian-24 <jurian.andriessen@gmail.com>
2023-11-17 12:49:47 +0100

    chore(fix): fixed misspelling of the attendence -> attendance

--- database/migrations/2023_11_16_161222_create_attendences_table.php
+++ database/migrations/2023_11_16_161222_create_attendences_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('attendences', function (Blueprint $table) {
+        Schema::create('attendances', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->uuid('employee_id');
             $table->integer('week_number');
@@ -26,6 +26,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('attendences');
+        Schema::dropIfExists('attendances');
     }
 };
