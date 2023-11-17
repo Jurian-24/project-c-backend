47b17d06ce666f286e517385183b701400684951
Jurian-24 <jurian.andriessen@gmail.com>
2023-11-17 12:48:47 +0100

    chore(database): added relations to the application

--- app/Models/CategorieImage.php
+++ app/Models/CategorieImage.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class CategorieImage extends Model
 {
     use HasFactory;
+
+    public function categorie() {
+        return $this->belongsTo(Categorie::class);
+    }
 }
--- app/Models/employee.php
+++ app/Models/employee.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function manager() {
+        return $this->hasOne(Manager::class);
+    }
+
+    public function company() {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function attendance() {
+        return $this->hasMany(Attendance::class);
+    }
 }
