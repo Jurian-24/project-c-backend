e7ca8f6331a1151d2e524f0a929dd901f1981d54
Jurian-24 <jurian.andriessen@gmail.com>
2023-11-17 11:58:12 +0100

    chore(build): added controllers and models

---
+++ app/Http/Controllers/CategorieImageController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\categorie_image;
+use Illuminate\Http\Request;
+
+class CategorieImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(categorie_image $categorie_image)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(categorie_image $categorie_image)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, categorie_image $categorie_image)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(categorie_image $categorie_image)
+    {
+        //
+    }
+}
