a0b63d5d9492b89fd16a8fea207499a190d56c81
Jurian-24 <jurian.andriessen@gmail.com>
2023-11-19 15:24:53 +0100

    chore(database): added a discount label table

---
+++ database/migrations/2023_11_19_125607_create_discount_labels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discount_labels', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('product_id');
+            $table->string('code');
+            $table->string('default_description');
+            $table->integer('count')->nullable();
+            $table->decimal('price')->nullable();
+            $table->integer('percentage')->nullable();
+            $table->integer('precise_percentage')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discount_labels');
+    }
+};
