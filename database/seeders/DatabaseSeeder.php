fcbca4a3fd1f50957a57635f8ba34336fc88d0bd
Jurian-24 <jurian.andriessen@gmail.com>
2023-11-23 02:26:38 +0100

    chore(feat): added testing users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +13,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // create employees
         \App\Models\User::factory(100)->create();
+
+        // create super admin
+        \App\Models\User::create([
+            'first_name' => 'Super',
+            'middle_name' => null,
+            'last_name' => 'Admin',
+            'role' => 'super_admin',
+            'password' => '$2y$12$5CeAFIio/UpcRhHUGyOTwun5P1zc5qCybOw7SIvv2kMDOSr6u8HVS', // password123
+            'email' => 'admin@buurtboer.nl',
+            'remember_token' => \Illuminate\Support\Str::random(10),
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
