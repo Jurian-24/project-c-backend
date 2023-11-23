f3b6b7c0eed5c50a60ba4038b7a13c6391db3efa
Jurian-24 <jurian.andriessen@gmail.com>
2023-11-23 02:24:02 +0100

    chore(feat): added new middlewares for better security

--- app/Http/Middleware/CheckCompanyAdmin.php
+++ app/Http/Middleware/CheckCompanyAdmin.php
@@ -1,25 +1,29 @@
 <?php
-declare(strict_types=1);
+
 namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class EnsureUserIsAuthenticated
+class CheckCompanyAdmin
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
         if (!auth()->check()) {
             // return 404 page
             return abort(404);
         }
+        if(auth()->user()->role !== 'company_admin' && auth()->user()->role !== 'super_admin') {
 
+            // return 404 page
+            return abort(404);
+        }
         return $next($request);
     }
 }
---
+++ app/Http/Middleware/CheckEmployee.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckEmployee
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!auth()->check()) {
+            // return 404 page
+            return abort(404);
+        }
+
+        if(auth()->user()->role !== 'employee') {
+            return abort(404);
+        }
+
+        return $next($request);
+    }
+}
