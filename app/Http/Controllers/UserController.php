09f67409d06ca982ae9276764fdc0da89dc57c97
Erik Maan <87411830+Failfan13@users.noreply.github.com>
2024-01-10 16:48:30 +0100

    Fix not getting right user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,7 +27,7 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        $user = User::find($id)->with('employee')->first();
+        $user = User::with('employee.company')->find($id);
 
         return response()->json([
             'user' => $user
